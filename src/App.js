9f6f93cc54275bf1f1ad2022da5e8d6fd7ef546f
FloridaFrank <frank.abong@certegy.com>
2022-02-10 21:30:30 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img
+					src={logo}
+					className="App-logo rotate"
+					alt="logo"
+					width="40"
+					height="40"
+				/>
+
+				<span className="architect">UI/UX Enterprise Architect</span>
+			</header>
+
+			<div className="main">
+				<h2>App Dev</h2>
+				<div className="project">project</div>
+				<div className="sub-main">
+					<h2>GitHub Pages demo-3</h2>
+				</div>
+			</div>
+			<footer>Frank Abong software &copy;2022</footer>
+		</div>
+	);
 }
 
 export default App;
